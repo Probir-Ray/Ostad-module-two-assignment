37e130798811139132c190455d50056fd8bf3375
Probir-Ray <somironroy.need@gmail.com>
2023-09-20 06:07:35 +0600

    update

--- task_1.php
+++ task_1.php
@@ -1,2 +1,13 @@
 <?php
 
+/**
+ * Task Title: Looping with Increment using a Function
+ * Created Date: 20/09/2023 (GMT + 6)
+ * Write a PHP function that uses a for loop to print all even numbers from 1 to 20, but with a
+ * step of 2. In other words, you should print 2, 4, 6, 8, 10, 12, 14, 16, 18, 20. The function
+ * should take the arguments like start as 1, end as 20 and step as 2. You must call the
+ * function to print.
+ * Also do the same using while loop and do-while loop also.
+**/
+
+
